36a6fc2c638556d1097ded7fb8590801eb79527f
OleksandrVaniuta <vaniutasa@gmail.com>
2023-05-04 15:01:51 +0300

    done

---
+++ src/redux/filterSlicer.js
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
+// import type { PayloadAction } from '@reduxjs/toolkit';
+
+export const filterSlice = createSlice({
+  name: 'filter',
+  initialState: { filter: ' ' },
+  reducers: {
+    addFilter: (state, action) => {
+      state.filter = action.payload;
+    },
+  },
+});
+
+export const { addFilter } = filterSlice.actions;
+
+export const getFilter = state => state.filter.filter;
